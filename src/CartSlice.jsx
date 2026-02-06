340b43b4b7c1b2adb4ffe97318e44f976e2e0beb
danieltelo95 <daniel.qui.tello@gmail.com>
2026-02-06 12:49:17 -0500

    CartSlice with reducer

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,16 +4,34 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
-  },
+  },  
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, cost, image } = action.payload;
+
+      const existingItem = state.items.find(item => item.name === name);
+      if(existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ name, cost, image, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const { name } = action.payload;
+      
+      if(state.items && state.items.length > 0) {
+       state.items = state.items.filter(item => item.name !== action.payload);
+    } else {
+        console.warn("Attempted to remove item from an empty cart.");
+      }
     },
     updateQuantity: (state, action) => {
-
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
     
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
